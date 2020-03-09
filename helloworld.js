f8b891b8bb8704977a734f05ed51e3e1abd42604
hotnewplanet <61372690+hotnewplanet@users.noreply.github.com>
2020-03-09 21:21:52 +0200

    Handle window resizing

--- helloworld.js
+++ helloworld.js
@@ -1,7 +1,9 @@
 let sprite;
+let aspect;
 
 function preload() {
   sprite = loadImage('img/Marx Engels Lenin.png');
+  aspect = sprite.height / sprite.width;
 }
 
 function setup() {
@@ -16,15 +18,25 @@ function draw() {
 
   translate(windowWidth/2, windowHeight/2);
 
+  R = windowWidth > windowHeight ? windowWidth/3 : windowHeight/3;
+
   beginShape();
   for (i = 1; i <= 10; i++) {
-    r = i % 2 == 0 ? windowWidth/4 : windowWidth/9;
+    r = i % 2 == 0 ? R : R*0.444;
     theta = -frameCount / 100;
     vertex(r * cos(theta + 2.0 * PI * i / 10), r * sin(theta + 2.0 * PI * i / 10));
   }
   endShape();
 
-  w = sprite.width + windowWidth/5 * cos(2.0 * PI * frameCount / 100);
-  h = sprite.height + windowWidth/5 * cos(2.0 * PI * frameCount / 100);
+  S = cos(2.0*PI*frameCount/100);
+  W = windowWidth > windowHeight ? windowWidth/3 + windowWidth/5*S : windowHeight/3 + windowHeight/5*S;
+
+  w = aspect * W;
+  h = W;
+
   image(sprite, -w/2, -h/2, w, h);
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
 }
