594ad1ebcd81d02425190b0f28137cf042c19425
hotnewplanet <61372690+hotnewplanet@users.noreply.github.com>
2020-03-25 17:21:06 +0200

    New stuff

---
+++ kraftwerk.js
@@ -0,0 +1,32 @@
+let song;
+let video;
+
+function preload() {
+  song = loadSound('snd/Kraftwerk.mp3');
+  video = createVideo(['vid/Dekki.mp4']);
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  video.size(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(0,0,0);
+  image(video, 0, 0);
+}
+
+function mousePressed() {
+  if (song.isPlaying()) {
+    video.pause();
+    song.pause();
+  } else {
+    video.loop();
+    song.play();
+  }
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+  video.size(windowWidth, windowHeight);
+}
